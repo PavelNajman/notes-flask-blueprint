8093efb0a70a1f07bf5691040abbad0245ba4426
Pavel Najman <najman.pavel@gmail.com>
2023-07-22 22:23:18 +0200

    Add length validation to schemas.

--- schemas.py
+++ schemas.py
@@ -1,6 +1,7 @@
 from marshmallow import Schema, fields
+from marshmallow.validate import Length
 
 class NoteSchema(Schema):
     id = fields.Int(dump_only=True)
-    title = fields.Str(required=True)
-    body = fields.Str(required=True)
+    title = fields.Str(required=True, validate=Length(max=1024))
+    body = fields.Str(required=True, validate=Length(max=8192))
